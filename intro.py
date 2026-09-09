8fbb618d8158128cd2b5ac01b76a9a38f04152fb
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-09-09 21:51:17 +0530

    py

---
+++ intro.py
@@ -0,0 +1,13 @@
+print("Hello, My Name Is Harsh")
+
+print("I Live in India")
+
+print("I am 12 years old")
+
+print("I am a student of class 7th")
+
+print(12)
+
+print(10 + 2)
+
+print("Hi I am Harsh", 12)
